394e4cd7df6fc1713016841995ce7f4c3c75fb43
ssyang <ben60523@gmail.com>
2020-04-10 18:37:03 +0800

    W.I.P: Show scan result

--- wifiscan/wifi_scan/scan.c
+++ wifiscan/wifi_scan/scan.c
@@ -6,26 +6,55 @@
 #include <wlanapi.h>
 #include <Windot11.h>
 #include <objbase.h>
-#include <wtypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+HANDLE hClient = NULL;
+PWLAN_INTERFACE_INFO pIfInfo = NULL;
+PDOT11_SSID pDotSSid = NULL;
 
+
+VOID CALLBACK notificationCallback(__in PWLAN_NOTIFICATION_DATA pNotifData, __in_opt PVOID pContext) {
+	if (pNotifData != NULL) {
+		switch (pNotifData->NotificationSource)
+		{
+		case WLAN_NOTIFICATION_SOURCE_ACM:
+			if (pNotifData->NotificationCode == wlan_notification_acm_connection_complete) {
+				WLAN_BSS_LIST WlanBssList;
+				int ret;
+				ret = WlanGetNetworkBssList(hClient, &pIfInfo->InterfaceGuid, pDotSSid, dot11_BSS_type_infrastructure, FALSE, NULL, &WlanBssList);
+				if (ret != ERROR_SUCCESS) {
+					wprintf(L"WlanGetNetworkBssList failed with error: %u\n", ret);
+					return 1;
+				}
+				else {
+					for (int i = 0; i < WlanBssList.dwNumberOfItems; i++) {
+						wprintf(L"SSID: %s\n", WlanBssList.wlanBssEntries->dot11Ssid);
+					}
+				}
+			}
+			break;
+		default:
+			break;
+		}
+	}
+}
 
 int wmain()
 {
-	HANDLE hClient = NULL;
+
 	DWORD dwMaxClient = 2;
 	DWORD dwCurVersion = 0;
 	DWORD dwResult = 0;
 	DWORD dwRetVal = 0;
 	GUID interfaceGuid;
+	DWORD dwPrevNotifType = 0;
+
 	unsigned int i, k = 0;
 
 	// variables used for WlanEnumInterfaces
 
 	PWLAN_INTERFACE_INFO_LIST pIfList = NULL;
-	PWLAN_INTERFACE_INFO pIfInfo = NULL;
 
 	PWLAN_CONNECTION_ATTRIBUTES pConnectInfo = NULL;
 	DWORD connectInfoSize = sizeof(WLAN_CONNECTION_ATTRIBUTES);
@@ -34,6 +63,12 @@ int wmain()
 	dwResult = WlanOpenHandle(dwMaxClient, NULL, &dwCurVersion, &hClient);
 	if (dwResult != ERROR_SUCCESS) {
 		wprintf(L"WlanOpenHandle failed with error: %u\n", dwResult);
+		return 1;
+	}
+
+	dwResult = WlanRegisterNotification(hClient, WLAN_NOTIFICATION_SOURCE_ALL, FALSE, notificationCallback, NULL, NULL, &dwPrevNotifType);
+	if (dwResult != ERROR_SUCCESS) {
+		wprintf(L"WlanRegisterNotification failed with error: %u\n", dwResult);
 		return 1;
 	}
 
@@ -47,7 +82,6 @@ int wmain()
 		wprintf(L"Current Index: %lu\n", pIfList->dwIndex);
 		for (i = 0; i < (int)pIfList->dwNumberOfItems; i++) {
 			pIfInfo = (WLAN_INTERFACE_INFO*)&pIfList->InterfaceInfo[i];
-			PDOT11_SSID pDotSSid = NULL;
 			WLAN_RAW_DATA WlanRawData;
 			dwResult = WlanScan(hClient, &pIfInfo->InterfaceGuid, pDotSSid, &WlanRawData, NULL);
 			if (dwResult != ERROR_SUCCESS) {
